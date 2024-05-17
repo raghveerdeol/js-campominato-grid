6a39cd2b28e028af5b506c2f4a847133fc9fb246
raghveerdeol <deolraghveer5@gmail.com>
2024-05-17 15:54:43 +0200

    creato articleList e aggiunto al dom. creato eventListener per aggiungere una classe al articleEl

---
+++ js/script.js
@@ -0,0 +1,24 @@
+
+// 9 quando clicco l'elemento aggiungo una classe che colora l'elemento e aggiungo console.log(article[index]);
+
+const sectionEl = document.querySelector('section#container');
+// inserisco un ciclo for che va da 0 a 100;
+for (let index = 0; index < 100; index++) {
+    // creo l'elemento article;
+    let articleEl = document.createElement("article");
+    // aggiungi una classe; 
+    articleEl.classList.add('casella');
+    // nel elemento article inserisco index;
+    articleEl.append(index + 1);
+    // article figlio di section;
+    sectionEl.appendChild(articleEl);
+    // aggiungo addEvenentListener (click);
+    articleEl.addEventListener("click", function bgClass() {
+        articleEl.classList.add('bg-casella');
+        console.log(articleEl.innerHTML);
+    });
+
+    console.log(articleEl);
+}
+
+
